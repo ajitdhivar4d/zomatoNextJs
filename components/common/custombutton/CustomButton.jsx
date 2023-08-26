3391ab2208cb8817a8b6d1e3ed79a5e4d3f97f9b
Ajit Dhivar <ajitdhivar4d@gmail.com>
2023-08-26 17:33:07 +0530

    resolve component import issue

--- components/common/custombutton/CustomButton.jsx
+++ components/common/custombutton/CustomButton.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import styles from "./customButton.module.scss"
+import styles from "./CustomButton.module.scss"
 import Image from "next/image"
 
 const CustomButton = ({ name, png, icon }) => {
