1ebb4c777989ca74f63560439cf52e252785ba9f
Ajit Dhivar <ajitdhivar4d@gmail.com>
2023-08-28 23:33:46 +0530

    favicon

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/app/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body className={inter.className}>
         <div className={styles.lgContainer}>
