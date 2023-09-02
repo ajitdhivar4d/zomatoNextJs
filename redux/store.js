5ca1e3ec62c39f8365bc47ba2900cf15e760d35c
Ajit Dhivar <ajitdhivar4d@gmail.com>
2023-09-02 21:19:29 +0530

    resolve image overflow

--- redux/store.js
+++ redux/store.js
@@ -2,14 +2,12 @@ import { configureStore } from "@reduxjs/toolkit"
 import categoryReducer from "./features/categorySlice"
 import deliRestaurantReducer from "./features/deliRestaurantSlice"
 import diningRestaurantReducer from "./features/diningRestaurantSlice"
-import oderPageInfoReducer from "./features/orderPageInfoSlice"
 
 const store = configureStore({
   reducer: {
     category: categoryReducer,
     deliRestaurant: deliRestaurantReducer,
     diningRestaurant: diningRestaurantReducer,
-    oderPageInfo: oderPageInfoReducer,
   },
 })
 
